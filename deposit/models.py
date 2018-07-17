65505daeba96503de5494304f11f6ebe64177853
Tomonori Ueno <t.uepon@gmail.com>
2018-07-17 21:30:28 +0900

    model added

--- deposit/models.py
+++ deposit/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
+
+
+class Account(models.Model):
+    aim = models.IntegerField('設定金額', max_length=10000000)
+    payment = models.IntegerField('入金額', max_length=10000000)
+    balance = models.IntegerField('残高', max_length=10000000)
+    date = models.DateTimeField('日付', default=timezone.now)
 
 # Create your models here.
